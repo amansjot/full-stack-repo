0221ec552d2419e4612b519f6159b07c9fde16e8
Aman Singh <amansjot@gmail.com>
2024-04-14 21:13:36 -0400

    added homepage ref

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
-import * as React from 'react';
-import { ChakraProvider, theme, Box } from '@chakra-ui/react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
+import * as React from "react";
+import { ChakraProvider, theme, Box } from "@chakra-ui/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 export const App = () => {
+  const baseUrl =
+    process.env.NODE_ENV === "production" ? "/full-stack-repo/" : "/";
+
   return (
     <ChakraProvider theme={theme}>
-      <Router>
+      <Router basename={baseUrl}>
         <Box textAlign="center" fontSize="xl">
           <Routes>
             <Route path="/" element={<Home />} />
